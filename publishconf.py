e610e9246a86de5e00b10cc2ccb477f1865333d3
Renato Oliveira <ren811@gmail.com>
2019-06-26 10:54:46 -0300

    change site url

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://wisefox.me'
+SITEURL = 'https://renatoliveira.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
